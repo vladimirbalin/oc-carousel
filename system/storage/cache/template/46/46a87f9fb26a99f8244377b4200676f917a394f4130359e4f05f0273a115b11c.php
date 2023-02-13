190272b880d3e3737e293109f799bd95c72794cc
vladimirbalin <vladimirzq@Gmail.com>
2023-02-13 17:49:52 +0700

    init

---
+++ system/storage/cache/template/46/46a87f9fb26a99f8244377b4200676f917a394f4130359e4f05f0273a115b11c.php
@@ -0,0 +1,70 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* common/footer.twig */
+class __TwigTemplate_f95807d5745fe0ca57d0b6ece6589c9925f4db03602e8cbd88da69d66044b17f extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<footer>
+  <div class=\"container\"><a href=\"http://opencart-russia.ru\" target=\"_blank\">";
+        // line 2
+        echo ($context["text_project"] ?? null);
+        echo "</a>|<a href=\"http://forum.opencart-russia.ru/threads/ustanovka.6044/\" target=\"_blank\">";
+        echo ($context["text_documentation"] ?? null);
+        echo "</a>|<a href=\"http://forum.opencart-russia.ru\" target=\"_blank\">";
+        echo ($context["text_support"] ?? null);
+        echo "</a><br />
+\t</div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "common/footer.twig", "");
+    }
+}
